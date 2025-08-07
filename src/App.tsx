05dd59602f91debd62fbe7df0513ce52198a9eb6
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-08-07 10:39:02 +0330

    feat: :sparkles: Implement actions section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
+    <div className="bg-background text-foreground min-h-screen">
       <Navbar />
-      Another
-    </>
+      <Container>
+        <section className="mt-8 mb-5 flex items-center justify-between">
+          <div className="flex gap-3">
+            <div className="bg-my-primary cursor-pointer rounded-lg px-3 py-1">
+              All
+            </div>
+            <div className="cursor-pointer rounded-lg px-3 py-1">Today</div>
+            <div className="cursor-pointer rounded-lg px-3 py-1">Completed</div>
+          </div>
+          <button className="bg-my-primary flex cursor-pointer items-center gap-2 rounded-lg px-3 py-2">
+            <img src="/plus-circle.png" alt="Plus" />
+            New Task
+          </button>
+        </section>
+      </Container>
+    </div>
   );
 };
 
